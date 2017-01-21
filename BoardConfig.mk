26428d1c3ad317b4a5effaa1b52ef2f503851432
Adarsh-MR <adarshmr1998@gmail.com>
2017-01-21 12:56:11 +0530

    update defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CONFIG := cyanogenmod_kenzo_defconfig
+TARGET_KERNEL_CONFIG := fractalx_kenzo_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
